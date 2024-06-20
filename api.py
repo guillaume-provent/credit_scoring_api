08696081ac82c768ed9fedff28ec346e9bf0627b
Guillaume Provent <131724707+guillaume-provent@users.noreply.github.com>
2024-06-20 13:30:50 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
-import os
 from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
+
 
 THRESHOLD = 0.6205400652376516
 
@@ -14,15 +14,18 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(current_directory, "model.pkl")
 model = joblib.load(model_path)
 
+
 # Chargement des données :
 data_path = os.path.join(current_directory, "data_api_medium.csv")
 data = pd.read_csv(data_path)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Récupération de l'identifiant du client à partir de la requête :
     sk_id_curr = request.json.get('SK_ID_CURR')
 
+    
     # Vérification de la présence de l'identifiant dans les données :
     if sk_id_curr not in data['SK_ID_CURR'].values:
         return jsonify({'error': 'Identifiant non trouvé'}), 404
@@ -37,11 +40,13 @@ def predict():
     # Verbalisation de la prédiction :
     if prediction == 0:
         result = 'DOSSIER ACCEPTE'
+    
     else:
         result = 'DOSSIER REFUSE'
 
     # Renvoi du résultat :
     return jsonify({'Identifiant': sk_id_curr, 'Dossier': result, 'Probabilite': proba, 'Seuil': THRESHOLD})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
